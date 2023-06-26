8cf41ba2d3474198624ca5a6f00448bcc46db975
Mohammed Aazam Sheikh H <109226447+MohammedAazam@users.noreply.github.com>
2023-06-26 15:36:03 +0000

    image added to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-export default function Page() {
+export default function Home() {
   return (
     <div className="h-full w-full overflow-y-scroll">
-      <main>
-        
+      <main className="bg">
+        <img
+          src="https://images.unsplash.com/photo-1574304904744-2e6919c5a2a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+          alt=""
+          className="w-full h-auto"
+        />
       </main>
     </div>
   );
